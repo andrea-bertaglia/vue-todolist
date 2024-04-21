67e6680abd3eeb6b205658a608eb9859a253d4b0
andrea-bertaglia <andreabertaglia@gmail.com>
2024-04-21 21:17:18 +0200

    feat: milestone 1

--- js/script.js
+++ js/script.js
@@ -3,7 +3,57 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      tasksPrev: [
+        {
+          text: "Quarterly Newsletter",
+          label: "Editorial",
+          done: true,
+        },
+      ],
+      tasksToday: [
+        {
+          text: "Recruiting blog post",
+          label: "Editorial",
+          done: true,
+        },
+        {
+          text: "Mobile app launch",
+          label: "Q1-Goals",
+          done: true,
+        },
+        {
+          text: "Interview John H.",
+          label: "Recruiting",
+          done: false,
+        },
+        {
+          text: "Summit update to mobile storefronts",
+          label: "Mobile",
+          done: true,
+        },
+      ],
+      tasksNext: [
+        {
+          text: "Schedule meeting with Alex",
+          label: "",
+          done: false,
+        },
+        {
+          text: "Homepage refresh",
+          label: "Website",
+          done: true,
+        },
+        {
+          text: "Onboard new Sales team members",
+          label: "Sales",
+          done: false,
+        },
+        {
+          text: "Review editorial calendar",
+          label: "Editorial",
+          done: true,
+        },
+      ],
     };
   },
 }).mount("#app");
